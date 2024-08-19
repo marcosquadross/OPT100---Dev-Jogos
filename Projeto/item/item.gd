0738b0180d9ae8396afdde3bb69bbda1a3b58e33
Joao-Girotto <joaogirotto34@gmail.com>
2024-08-19 14:28:46 -0300

    Peixe Funcionando

--- Projeto/item/item.gd
+++ Projeto/item/item.gd
@@ -9,7 +9,12 @@ class_name Item
 func _ready() -> void:
 	var offset: int = int(global_position.x) % 10
 	_player.seek( (_player.current_animation_length / 10) * offset)
+	
 
 
 func get_value() -> int:
 	return value
+
+
+func _on_collision_shape_2d_ready() -> void:
+	pass # Replace with function body.
