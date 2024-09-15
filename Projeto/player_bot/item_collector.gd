d71b35d175a56a9674d1c5667d01d71a58ecd2d7
marcosquadross <marcosdquadros@gmail.com>
2024-09-15 20:30:06 -0300

    Fix fish collidion

--- Projeto/player_bot/item_collector.gd
+++ Projeto/player_bot/item_collector.gd
@@ -8,12 +8,11 @@ var power_up_time = 10.0
 signal item_collected(item: Item)
 
 func _on_area_entered(item) -> void:
-	print(item.name)
 	if item is Item:
 		item_collected.emit(item)
 		_total_items += item.get_value()
 		gc.update_hud_items(_total_items)
-		collect.play()
+		item.queue_free()
 	elif item is KeyItem:
 		player.has_key = true
 		item.get_key()
